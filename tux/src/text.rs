d1f6e7ad090343747b7f56d680a8f7195c3542ff
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-03-05 16:02:42 -0300

    feat(text): add `trim_lines` function
    Removes extraneous white-space from an iterator of lines.

--- tux/src/text.rs
+++ tux/src/text.rs
@@ -6,11 +6,24 @@
 /// trailing empty lines.
 pub fn lines<S: AsRef<str>>(input: S) -> Vec<String> {
 	let input = input.as_ref().trim_end();
-	let iter = LinesIterator { input, index: 0 };
-	iter.map(|x| x.trim_end())
-		.skip_while(|x| x.len() == 0)
-		.map(|x| x.into())
-		.collect()
+	let output = LinesIterator { input, index: 0 };
+	trim_lines(output).collect()
+}
+
+/// Removes extraneous white-space from a sequence of lines.
+///
+/// This will trim spaces from the end of lines, and remove leading and
+/// trailing empty lines.
+pub fn trim_lines<'a, I>(input: I) -> impl Iterator<Item = String>
+where
+	I: IntoIterator,
+	I::Item: AsRef<str>,
+{
+	let output = input
+		.into_iter()
+		.map(|x| x.as_ref().trim().to_string())
+		.skip_while(|x| x.len() == 0);
+	TrimEndIterator::wrap(output)
 }
 
 struct LinesIterator<'a> {
@@ -41,6 +54,48 @@ impl<'a> Iterator for LinesIterator<'a> {
 		} else {
 			self.index += input.len();
 			Some(input)
+		}
+	}
+}
+
+struct TrimEndIterator<T: Iterator<Item = String>> {
+	inner: T,
+	current_run_of_empty_strings: usize,
+	non_empty_string_after_run: Option<String>,
+}
+
+impl<T: Iterator<Item = String>> TrimEndIterator<T> {
+	pub fn wrap(inner: T) -> Self {
+		TrimEndIterator {
+			inner,
+			current_run_of_empty_strings: 0,
+			non_empty_string_after_run: None,
+		}
+	}
+}
+
+impl<T: Iterator<Item = String>> Iterator for TrimEndIterator<T> {
+	type Item = String;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		loop {
+			if self.non_empty_string_after_run.is_some() {
+				if self.current_run_of_empty_strings > 0 {
+					self.current_run_of_empty_strings -= 1;
+					return Some(String::new());
+				}
+				return std::mem::take(&mut self.non_empty_string_after_run);
+			}
+
+			if let Some(next) = self.inner.next() {
+				if next.len() > 0 {
+					self.non_empty_string_after_run = Some(next);
+				} else {
+					self.current_run_of_empty_strings += 1;
+				}
+			} else {
+				return None;
+			}
 		}
 	}
 }
@@ -83,5 +138,40 @@ mod tests {
 	fn lines_trim_line_ends() {
 		let out = lines("l1  \nl2\t\r\nl3\t \rl4  ");
 		assert_eq!(out, vec!["l1", "l2", "l3", "l4"]);
+	}
+
+	#[test]
+	fn trim_lines_returns_non_empty_lines() {
+		let input = vec!["1", "2", "3"];
+		let out = trim_lines(input.iter()).collect::<Vec<_>>();
+		assert_eq!(out, vec!["1", "2", "3"]);
+	}
+
+	#[test]
+	fn trim_lines_skips_leading_empty_lines() {
+		let input = vec!["", "  ", "\t", "first non-empty", "second"];
+		let out = trim_lines(input.iter()).collect::<Vec<_>>();
+		assert_eq!(out, vec!["first non-empty", "second"]);
+	}
+
+	#[test]
+	fn trim_lines_trim_line_ends() {
+		let input = vec!["l1  ", "l2\t", "l3\t ", "l4  "];
+		let out = trim_lines(input.iter()).collect::<Vec<_>>();
+		assert_eq!(out, vec!["l1", "l2", "l3", "l4"]);
+	}
+
+	#[test]
+	fn trim_lines_removes_trailing_empty_lines() {
+		let input = vec!["first", "last non-empty", "", "  ", "\t"];
+		let out = trim_lines(input.iter()).collect::<Vec<_>>();
+		assert_eq!(out, vec!["first", "last non-empty"]);
+	}
+
+	#[test]
+	fn trim_lines_does_not_remove_empty_lines_from_the_middle() {
+		let input = vec!["1x", "", "2x", "", "", "3x", "", "", "", "!"];
+		let out = trim_lines(input.iter()).collect::<Vec<_>>();
+		assert_eq!(out, vec!["1x", "", "2x", "", "", "3x", "", "", "", "!"]);
 	}
 }
