87c04d6899e6b788682d2bd79df5f8a148c1194f
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-03-20 12:15:40 -0300

    docs(exec): minor fixes to doc comments

--- tux/src/exec.rs
+++ tux/src/exec.rs
@@ -1,12 +1,12 @@
 use std::process::Command;
 
-/// Returns a `Command` for running a binary from the project (i.e. a binary
+/// Returns a [`Command`] for running a binary from the project (i.e. a binary
 /// built by Cargo).
 ///
 /// This is intended to be used by integration tests that need to run one of
 /// the crate's binaries.
 ///
-/// See also `get_command_output` and `run_bin`.
+/// See also [`get_process_output`] and [`run_bin`].
 pub fn get_bin(name: &str) -> Command {
 	// Cargo generates integration tests at `target/debug/deps`
 	let mut exe_path = std::env::current_exe().expect("getting current executable filename");
@@ -28,7 +28,7 @@ pub fn get_bin(name: &str) -> Command {
 	Command::new(exe_path)
 }
 
-/// Convenience function combining `get_bin` and `get_command_output`.
+/// Convenience function combining [`get_bin`] and [`get_process_output`].
 pub fn run_bin(cmd: &str, args: &[&str]) -> String {
 	let mut cmd = get_bin(cmd);
 	cmd.args(args);
@@ -38,8 +38,8 @@ pub fn run_bin(cmd: &str, args: &[&str]) -> String {
 }
 
 /// Utility function to retrieve the standard output of a process from
-/// the [`std::process::Output`] as a string, while validating the exit
-/// code and stderr output.
+/// the [`std::process::Output`] while validating the exit status and
+/// error output.
 ///
 /// This will panic if the process exit status is non-zero or if any
 /// error output is generated.
